75c8eac1e801dd5e334d5ddec4a8be360e830f6a
Anders <1086088@ucn.dk>
2021-08-15 17:33:46 +0200

    JS page(title, nav) + CSS
    Ready to start making functions for creating HTML according to URL.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,6 @@
 //UCN MMDA0920
 
 
-console.log("        /\\       _______   ___    __ ")
-console.log("       /  \\     |   ____| |   \\  |  |")
-console.log("      / /\\ \\    |  |____  |    \\ |  |")
-console.log("     / /__\\ \\   |   ____| |  |\\ \\|  |")
-console.log("    / /____\\ \\  |  |____  |  | \\    |")
-console.log("   /_/      \\_\\ |_______| |__|  \\___|")
-
                             // IKKE NØDVENDIG; MEN NICE!
                             function footerPosition() {
                                 let
@@ -33,20 +26,102 @@ document.addEventListener("scroll", onScroll)
 
 // Create Sites
     const
-    cURL = document.location.href;
+    thisURL = document.location.href;
+    //Changes URL without Reloading
+    // function URLcreate () {
+    //     window.history.pushState("stateObj", "Title", "index.html/ommig");
+    // }
 
-    function URLcreate () {
-        window.history.pushState("stateObj", "Title", "index.html/ommig");
-    }
-    function HTMLcreate () {
-        if (cURL.includes("?ommig")) {
-            console.log("AYA")
+    function titleCreate (title) {
+        let 
+        newTitle = title.charAt(0).toUpperCase() + title.slice(1);
+        // Makes sure to split up "ommig"
+        if (title === "om-mig") {
+            let
+            newSplit = title.split("-"),
+            splitOne = newSplit[0].charAt(0).toUpperCase() + newSplit[0].slice(1),
+            splitTwo = newSplit[1].charAt(0).toUpperCase() + newSplit[1].slice(1);
+            newTitle = splitOne + " " + splitTwo; 
         }
+        //Changes the Title
+        document.title = newTitle + " - Anders Erik Nissen - Portfolio"
+    }
+    function GlobalNAVactive (active) {
+        let
+        list = document.querySelectorAll(".globalNAVli"),
+        act = active;
+        //If the the site either SNV or Engelrod set active on "projekter"
+        if (act === "snv") {
+            act = "projekter";
+        }
+        if (act === "englerod") {
+            act = "projekter";
+        }
+        //Check each item in the Array.
+        list.forEach( each => {
+            let 
+            //Check each href + slice "?"
+            att = each.getAttribute("href"),
+            attCut = att.slice(1);
+            // First remove if there is any .active_nav, then add to the matching nav-li-element.
+            each.classList.remove("active_nav");
+            if (attCut === act) {
+                each.classList.add("active_nav");
+            }
+        })
+
+    }
+    function SITEcreate () {
+        let 
+        checkSplit = thisURL.split("?"),
+        check = checkSplit[1];
+    
+        switch (check) {
+
+            case undefined:
+                titleCreate("Forside");
+                console.log("false!!")
+                break;
+
+            case "om-mig":
+                titleCreate(check);
+                GlobalNAVactive(check);
+                console.log(check)
+                break;
+
+            case "projekter":
+                titleCreate(check);
+                GlobalNAVactive(check);
+                console.log(check)
+                break;
+
+            case "kontakt":
+                titleCreate(check);
+                GlobalNAVactive(check);
+                console.log(check)
+                break;
+
+            case "snv":
+                titleCreate(check);
+                GlobalNAVactive(check);
+                console.log(check)
+                break;
+
+            case "englerod":
+                titleCreate(check);
+                GlobalNAVactive(check);
+                console.log(check)
+                break;
+                
+            default:
+                console.log("DEFAULT: HTML-CREATE")
+        }
+        // "Forside", ommig, projekter, kontakt, snv, englerod
         
     }
-    HTMLcreate();
+    SITEcreate();
 
-
+// window.addEventListener("load", ()=> {console.log(window.location.href)})
 
 
 
@@ -123,4 +198,9 @@ const
     // .then(json => console.log(json))
 
 
- 
+    console.log("        /\\       _______   ___    __ ")
+    console.log("       /  \\     |   ____| |   \\  |  |")
+    console.log("      / /\\ \\    |  |____  |    \\ |  |")
+    console.log("     / /__\\ \\   |   ____| |  |\\ \\|  |")
+    console.log("    / /____\\ \\  |  |____  |  | \\    |")
+    console.log("   /_/      \\_\\ |_______| |__|  \\___|")
